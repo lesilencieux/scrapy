25b6315a729679164aa96ffa9f8a4135d11d67e0
hilaire <hilaire@right-com.com>
2020-07-06 10:27:48 +0100

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:alpine3.6
-MAINTAINER Mouhoutassim BELLO (mouhoutassim.bello@rintio.com)
+MAINTAINER Hilaire Ahotonnagnon (hilaireahotonnagnon@gmail.com)
 RUN pip install --upgrade pip
 COPY . /app
 WORKDIR /app
--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ docker stop app_scripy
 docker rm app_scripy
 docker rmi app_scripy_img
 docker build -t app_scripy_img .
-docker run -p 8585:8585 --name app_scripy --restart always -d app_scripy_img
+docker run -p 8385:8385 --name app_scripy --restart always -d app_scripy_img
